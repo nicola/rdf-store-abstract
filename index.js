7a7df908c14d97272b90267a42634942db8e38e1
bergi <bergi@axolotlfarm.org>
2015-10-18 23:27:01 +0200

    refactoring using standard code style + added tests

--- index.js
+++ index.js
@@ -1,53 +1,74 @@
-'use strict';
+function AbstractStore () {
+}
 
-var Abstract = function () {
-  var self = this;
+AbstractStore.prototype.add = function () {
+  throw new Error('not implemented')
+}
 
-  this.match = function (iri, subject, predicate, object, callback, limit) {
-    self.graph(iri, function (graph) {
-      if (graph == null) {
-        callback(null);
-      } else {
-        callback(graph.match(subject, predicate, object, limit));
-      }
-    });
-  };
+AbstractStore.prototype.delete = function () {
+  throw new Error('not implemented')
+}
 
-  this.merge = function (iri, graph, callback) {
-    self.graph(iri, function (existing) {
-      var merged = graph;
+AbstractStore.prototype.graph = function () {
+  throw new Error('not implemented')
+}
 
-      if (existing != null) {
-        merged = existing.addAll(graph);
-      }
+AbstractStore.prototype.match = function (subject, predicate, object, iri, callback, limit) {
+  var self = this
 
-      self.add(merged, callback);
-    });
-  };
+  callback = callback || function () {}
 
-  this.remove = function (iri, graph, callback) {
-    self.graph(iri, function (existing) {
-      if (existing != null) {
-        self.add(rdf.Graph.difference(existing, graph), function (added) {
-          callback(added != null);
-        });
-      } else {
-        callback(true);
-      }
-    });
-  };
+  return self.graph(iri).then(function (graph) {
+    if (graph) {
+      graph = graph.match(subject, predicate, object, limit)
+    }
 
-  this.removeMatches = function (iri, subject, predicate, object, callback) {
-    self.graph(iri, function (existing) {
-      if (existing != null) {
-        self.add(iri, existing.removeMatches(subject, predicate, object), function (added) {
-          callback(added != null);
-        });
-      } else {
-        callback(true);
-      }
-    });
-  };
-};
+    callback(null, graph)
 
-module.exports = Abstract;
+    return graph
+  })
+}
+
+AbstractStore.prototype.merge = function (iri, graph, callback) {
+  var self = this
+
+  callback = callback || function () {}
+
+  return self.graph(iri).then(function (existing) {
+    if (existing) {
+      existing.addAll(graph)
+    } else {
+      existing = graph
+    }
+
+    return self.add(iri, existing, callback)
+  }).then(function () {
+    return graph
+  })
+}
+
+AbstractStore.prototype.remove = function (iri, graph, callback) {
+  var self = this
+
+  callback = callback || function () {}
+
+  return self.graph(iri).then(function (existing) {
+    if (existing) {
+      return self.add(iri, existing.difference(graph))
+    }
+  }).then(callback)
+}
+
+AbstractStore.prototype.removeMatches = function (subject, predicate, object, iri, callback) {
+  var self = this
+
+  callback = callback || function () {}
+
+  return self.graph(iri).then(function (existing) {
+    if (existing) {
+      return self.add(iri, existing.removeMatches(subject, predicate, object))
+    }
+  }).then(callback)
+}
+
+module.exports = AbstractStore
